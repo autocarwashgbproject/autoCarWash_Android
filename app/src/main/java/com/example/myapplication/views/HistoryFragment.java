e289840527e48b9c4104600ce7efca93580d69a9
Malomnogo <auricamercer@gmail.com>
2019-08-16 00:09:07 +0300

    добавил recyclerview добавил временную заглушку до появления бд

--- app/src/main/java/com/example/myapplication/views/HistoryFragment.java
+++ app/src/main/java/com/example/myapplication/views/HistoryFragment.java
@@ -8,12 +8,22 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.adapters.HistoryAdapter;
+import com.example.myapplication.models.History;
+import com.example.myapplication.models.HistoryList;
 
-public class HistoryFragment extends MvpAppCompatFragment {
+import java.util.List;
+
+public class HistoryFragment extends MvpAppCompatFragment implements HistoryIF {
+    private RecyclerView recyclerView;
+    private HistoryAdapter adapter;
+    private List<History> histories;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -32,8 +42,13 @@ public class HistoryFragment extends MvpAppCompatFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.history_fragment, container, false);
+        ((MainActivity) getActivity()).getBottomNavigationView().setVisibility(View.VISIBLE);
+        histories = HistoryList.getInstance().getHistories();
 
-        ((MainActivity)getActivity()).getBottomNavigationView().setVisibility(View.VISIBLE);
+        recyclerView = view.findViewById(R.id.list_history);
+        adapter = new HistoryAdapter(histories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
 
         return view;
     }
